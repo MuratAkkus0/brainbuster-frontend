bf105c69e1f1b79d55b7e5624b33aba811e6a385
Murat Akkus <akkusmurat123@gmail.com>
2025-10-02 00:26:25 +0200

    Logo.tsx made responsive.

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,8 +1,8 @@
 export const Logo = () => {
   return (
-    <div className="flex items-center justify-center max-w-fit max-h-fit">
-      <div className="text-6xl font-bold text-theme-accent relative">
-        <p className="[clip-path:polygon(0_0,100%_0,100%_40%,0_60%)] absolute -top-1 -left-2 ">
+    <div className="flex items-center justify-center max-w-fit max-h-fit p-2">
+      <div className="text-4xl sm:text-5xl md:text-6xl font-bold text-theme-accent relative">
+        <p className="[clip-path:polygon(0_0,100%_0,100%_40%,0_60%)] absolute -top-1 -left-1 ">
           <span>Brain</span>
           <span className="text-theme-main-text">Buster</span>
         </p>
