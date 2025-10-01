b790cc5da2c9752e192865b22f75afc216639721
Murat Akkus <akkusmurat123@gmail.com>
2025-10-01 21:27:52 +0200

    Edit Logo component

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,6 +1,6 @@
 export const Logo = () => {
   return (
-    <>
+    <div className="flex items-center justify-center max-w-fit max-h-fit">
       <div className="text-6xl font-bold text-theme-accent relative">
         <p className="[clip-path:polygon(0_0,100%_0,100%_40%,0_60%)] absolute -top-1 -left-2 ">
           <span>Brain</span>
@@ -11,6 +11,6 @@ export const Logo = () => {
           <span className="text-theme-main-text">Buster</span>
         </p>
       </div>
-    </>
+    </div>
   );
 };
