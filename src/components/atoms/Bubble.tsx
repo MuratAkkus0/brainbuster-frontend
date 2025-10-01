cccb121061919d86d9b4c9c8e8d37cdec2c01aa3
Murat Akkus <akkusmurat123@gmail.com>
2025-10-02 01:30:18 +0200

    Bubble.tsx created.

---
+++ src/components/atoms/Bubble.tsx
@@ -0,0 +1,17 @@
+export const Bubble = ({
+  className = "border-theme-accent bg-theme-dark-bg",
+}: {
+  className?: string;
+}) => {
+  return (
+    <>
+      <div className="max-w-fit max-h-fit bg-transparent absolute top-0 left-0 z-0">
+        <div
+          className={
+            "size-48 -ml-[2.4rem] -mt-[2.4rem] border-8 rounded-full [clip-path:polygon(20%_20%,100%_20%,100%_100%,20%_100%)] bg-theme-accent border-theme-accent sm:bg-theme-accent sm:border-theme-dark-bg"
+          }
+        ></div>
+      </div>
+    </>
+  );
+};
