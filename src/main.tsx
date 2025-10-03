a8092768db08e07d28edf0f51a8b8888bfa737ee
Murat Akkus <akkusmurat123@gmail.com>
2025-10-03 22:02:11 +0200

    Add admin dashboard.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { HomePageView } from "./views/HomePageView.tsx";
 import { LoginPageView } from "./views/LoginPageView.tsx";
 import { RegisterFormView } from "./views/RegisterFormView.tsx";
+import AdminDashboard from "./components/molecules/AdminDashboard.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
       <Route path="/" element={<HomePageView />} />
       <Route path="/login" element={<LoginPageView />} />
       <Route path="/register" element={<RegisterFormView />} />
+      <Route path="/admin/dashboard" element={<AdminDashboard />} />
     </Routes>
   </BrowserRouter>
 );
