aa1d484c7a74575ce1aa40f9290d0d8c501749c4
Murat Akkus <128329402+MuratAkkus0@users.noreply.github.com>
2025-10-03 20:04:10 +0200

    Redirect home on click ,make user select none and add cursor pointer … (#13)
    * Redirect home on click ,make user select none and add cursor pointer class
    
    * remove unused e param

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,6 +1,13 @@
+import { useNavigate } from "react-router";
+
 export const Logo = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="flex items-center justify-center max-w-fit max-h-fit p-2">
+    <div
+      onClick={() => navigate("/")}
+      className="flex items-center justify-center max-w-fit max-h-fit p-2 select-none cursor-pointer"
+    >
       <div className="text-4xl sm:text-5xl md:text-6xl font-bold text-theme-accent relative">
         <p className="[clip-path:polygon(0_0,100%_0,100%_40%,0_60%)] absolute -top-1 -left-1 ">
           <span>Brain</span>
