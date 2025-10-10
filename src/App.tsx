7da2f57c272956c1ccdcca47d4d5340cfc1eed23
Murat Akkus <akkusmurat123@gmail.com>
2025-10-10 11:05:55 +0200

    Create App component again and share app logic between main and app

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,30 @@ import { LoginPageView } from "./views/LoginPageView.tsx";
 import { RegisterFormView } from "./views/RegisterFormView.tsx";
 import { AdminDashboardView } from "./views/AdminDashboardView.tsx";
 import { GameOverviewView } from "./views/GameOverviewView.tsx";
-import { store } from "@/store/store.ts";
-import { Provider } from "react-redux";
+
+import { LogoutView } from "./views/LogoutView.tsx";
+import { RequireAuth } from "./components/RequireAuth.tsx";
+import { NotFoundView } from "./views/NotFoundView.tsx";
 
 export const App = () => {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePageView />} />
-          <Route path="/login" element={<LoginPageView />} />
-          <Route path="/register" element={<RegisterFormView />} />
+    <BrowserRouter>
+      <Routes>
+        {/* public */}
+        <Route path="/" element={<HomePageView />} />
+        <Route path="/login" element={<LoginPageView />} />
+        <Route path="/register" element={<RegisterFormView />} />
+
+        {/* has to be authenticated */}
+        <Route element={<RequireAuth />}>
+          <Route path="/logout" element={<LogoutView />} />
           <Route path="/admin/dashboard" element={<AdminDashboardView />} />
           <Route path="/quiz" element={<GameOverviewView />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+        </Route>
+
+        {/* not found */}
+        <Route path="*" element={<NotFoundView />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App.tsx";
-
+import { store } from "@/store/store.ts";
+import { Provider } from "react-redux";
 createRoot(document.getElementById("root")!).render(
   <>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </>
 );
