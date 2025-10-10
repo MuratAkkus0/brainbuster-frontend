7ccb4a3da2940b18fc5234fc34bd8974f086e1dc
Murat Akkus <akkusmurat123@gmail.com>
2025-10-10 11:00:26 +0200

    Refactor: Hook refactored .

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -2,14 +2,7 @@ import type { UserState } from "@/store/slices/userSlice";
 import type { RootState } from "@/store/store";
 import { useSelector } from "react-redux";
 
-interface AuthHook {
-  (): CallableFunction;
-}
-
-export const useAuth: AuthHook = () => {
-  const getAuth = () => {
-    const user = useSelector((store: RootState): UserState => store.user);
-    return user;
-  };
-  return getAuth;
+export const useAuth = () => {
+  const user = useSelector((store: RootState): UserState => store.user);
+  return { user, isAuthenticated: !!user.user };
 };
