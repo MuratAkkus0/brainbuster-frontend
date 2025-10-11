aed258d518315629af4a717bc57aa9ab1eda3a18
Murat Akkus <akkusmurat123@gmail.com>
2025-10-11 16:18:47 +0200

    Refactor hooks and create useRegister hook.

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -1,30 +1,30 @@
-import { handleLogin } from "@/store/slices/userSlice";
+import { handleLogin, setUser } from "@/store/slices/userSlice";
 import type { LoginObjectModel } from "@/types/models/Auth/LoginObjectModel";
 import { useDispatch } from "react-redux";
-import type { AppDispatch } from "@/store/store";
+import type { AppDispatch, RootState } from "@/store/store";
 import type { UserModel } from "@/types/models/Auth/UserModel";
 
 interface LoginHook {
   (): CallableFunction;
 }
 
+interface LoginHookFunction {
+  (loginCredential: LoginObjectModel): Promise<UserModel | void>;
+}
+
 export const useLogin: LoginHook = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const login = async (
-    loginCredential: LoginObjectModel
-  ): Promise<UserModel | void> => {
+  const login: LoginHookFunction = async (loginCredential) => {
     console.log("login useLogin ");
-    dispatch(handleLogin(loginCredential)).then((item) => {
-      const loginData = item.payload as UserModel;
-
-      console.log(loginData);
-
-      if (loginData.user) {
-        return loginData.user;
-      } else {
+    let res = dispatch(handleLogin(loginCredential))
+      .then((item) => {
+        const loginData = item.payload as UserModel;
         return loginData;
-      }
-    });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    return res;
   };
   return login;
 };
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from "./auth/useAuth";
 export * from "./auth/useLogin";
 export * from "./auth/useLogout";
+export * from "./auth/useRegister";
