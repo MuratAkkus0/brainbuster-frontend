4634f06300cba751d7b24ee2d376f35a5149ce72
Murat Akkus <akkusmurat123@gmail.com>
2025-10-12 14:40:14 +0200

    User Dashboard created and sidebar changed with collapsable sidebar .

---
+++ src/views/UserDashboardView.tsx
@@ -0,0 +1,11 @@
+import UserDashboard from "@/components/organisms/UserDashboard";
+
+type Props = {};
+
+export function UserDashboardView({}: Props) {
+  return (
+    <>
+      <UserDashboard />
+    </>
+  );
+}
