620f28f1345a4c910eeec1cdfe4ac2477d3886d5
Murat Akkus <akkusmurat123@gmail.com>
2025-10-13 00:14:01 +0200

    BuxFix RequireAuth component.

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -3,24 +3,31 @@ import { Navigate, Outlet, useLocation } from "react-router";
 
 export const RequireAuth = () => {
   const { user } = useAuth();
+  const location = useLocation();
+
   const localStorageToken = localStorage.getItem("ut");
   const storeToken = user.user?.token;
-  const location = useLocation();
-  let token = "";
-
+  let token;
+  let isAuth = false;
   if (!storeToken && localStorageToken) {
     token = localStorageToken;
-  } else if (storeToken && localStorageToken) {
+  } else if (storeToken && !localStorageToken) {
+    token = localStorageToken;
+  }
+  if (storeToken && localStorageToken) {
     if (storeToken !== localStorageToken) {
-      return (
-        <Navigate to={"/logout"} state={{ from: location.pathname }} replace />
-      );
+      isAuth = false;
+    } else {
+      isAuth = true;
     }
   }
-
   return (
     <>
-      <Outlet />
+      {isAuth ? (
+        <Outlet />
+      ) : (
+        <Navigate to={"/logout"} state={{ from: location.pathname }} replace />
+      )}
     </>
   );
 };
