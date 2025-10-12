5f7767d6fcfdcebc737176550e0a6d0a1ff78304
Murat Akkus <akkusmurat123@gmail.com>
2025-10-12 19:32:46 +0200

    Persistant store installed and configured

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App.tsx";
-import { store } from "@/store/store.ts";
+import { persistor, store } from "@/store/store.ts";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 createRoot(document.getElementById("root")!).render(
   <>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </>
 );
