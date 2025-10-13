bf862c26cb2d42f42d31f6ac8c447a2b6e565d1a
Murat Akkus <akkusmurat123@gmail.com>
2025-10-13 13:55:01 +0200

    cleaun old code and make it funtional using sp game hooks

--- src/components/organisms/GameOverview.tsx
+++ src/components/organisms/GameOverview.tsx
@@ -1,67 +1,174 @@
 import { cn } from "@/lib/utils";
 import { UserAvatar } from "../atoms/UserAvatar";
+import {
+  useEffect,
+  useState,
+  type HTMLAttributes,
+  type MouseEventHandler,
+} from "react";
+import { useAuth } from "@/hooks";
+import type { UserModel } from "@/types/models/Auth/UserModel";
+import { useSPQuiz } from "@/hooks/game/useSPQuiz";
 
 const quizChoiceTagArr = ["A", "B", "C", "D"];
-const quizs = [
-  {
-    question: "question",
-    choices: ["choise 1", "choise 2", "choise 3", "choice 4"],
-  },
-  {
-    question: "question",
-    choices: ["choise 1", "choise 2", "choise 3", "choice 4"],
-  },
-  {
-    question: "question",
-    choices: ["choise 1", "choise 2", "choise 3", "choice 4"],
-  },
-  {
-    question: "question",
-    choices: ["choise 1", "choise 2", "choise 3", "choice 4"],
-  },
-];
 
-const UserAvatarContainer = ({ position }: { position: "right" | "left" }) => {
+const UserAvatarContainer = ({
+  user,
+  position,
+}: {
+  user?: UserModel;
+  position: "right" | "left";
+}) => {
   return (
     <>
       <div
-        className={cn(
-          "absolute bg-theme-main-bg p-2",
-          position == "left" ? "left-4" : "right-4"
-        )}
+        className={cn("absolute ", position == "left" ? "left-4" : "right-4")}
       >
-        <UserAvatar />
+        <div className="bg-theme-main-bg p-2">
+          <UserAvatar />
+        </div>
+        {user && (
+          <div className="w-full p-2 text-center font-bold">
+            {user.user.username}
+          </div>
+        )}
       </div>
     </>
   );
 };
 
-const QuizChoice = ({ tag, children }: { tag: string; children: string }) => {
+interface QuizChoiceProps extends HTMLAttributes<HTMLDivElement> {
+  onClick: MouseEventHandler<HTMLDivElement>;
+  tag: string;
+  children: string;
+}
+
+const QuizChoice: React.FC<QuizChoiceProps> = ({
+  onClick,
+  tag,
+  children,
+  ...props
+}) => {
   return (
-    <div className="grid grid-cols-12 grid-rows-1 border-2 items-center justify-items-center cursor-pointer">
+    <div
+      {...props}
+      onClick={onClick}
+      className="grid grid-cols-12 grid-rows-1 border-2 items-center justify-items-center cursor-pointer"
+    >
       <div className="col-span-3 bg-theme-accent w-full h-full flex items-center justify-center font-bold text-2xl">
         {tag}
       </div>
-      <div className="col-span-9 text-theme-main-text text-lg justify-self-start px-4">
+      <div className="col-span-9 text-theme-main-text text-lg md:text-xl justify-self-start px-4">
         {children}
       </div>
     </div>
   );
 };
-
+interface Choice {
+  choiceId: string;
+  text: string;
+}
 export const GameOverview = () => {
+  const { user } = useAuth().user;
+  const [quiz, setQuiz] = useState({
+    sessionId: "",
+    answerId: "",
+    state: "",
+    currentQuestion: {
+      choices: [] as Choice[],
+      index: 0,
+      prompt: "",
+      questionId: 0,
+    },
+    totalQuestion: 0,
+  });
+  const {
+    createSession,
+    startSession,
+    answerQuestion,
+    getCurrentQuestion,
+    getCurrentSessionInfo,
+  } = useSPQuiz();
+  const quizLS: Partial<typeof quiz> = JSON.parse(
+    localStorage.getItem("quiz") || "{}"
+  );
+  const gameMode = localStorage.getItem("qm");
+
+  useEffect(() => {
+    console.log(quizLS);
+    if (!quizLS || !quizLS?.sessionId) {
+      console.log(true);
+      createSession("test", "easy", 10).then((res: any) => {
+        console.log(res);
+        setQuiz((prev) => ({
+          ...prev,
+          sessionId: res.sessionId,
+          state: res.state,
+          totalQuestion: res.totalQuestions,
+        }));
+      });
+    }
+    return () => localStorage.removeItem("quiz");
+  }, []);
+
+  useEffect(() => {
+    if (quiz.sessionId && quiz.state != "RUNNING") {
+      startSession(quiz.sessionId).then((res: any) => {
+        console.log(res);
+        setQuiz((prev) => ({
+          ...prev,
+          currentQuestion: res.current,
+          state: res.state,
+        }));
+      });
+      if (quiz.sessionId && quiz.state == "RUNNING") {
+        getCurrentQuestion(quiz.sessionId).then((res: any) => {
+          console.log("------------------");
+          console.log(res);
+        });
+      }
+    }
+    if (quiz.sessionId) {
+      getCurrentSessionInfo(quiz.sessionId).then((res: any) => {
+        console.log("..................");
+        console.log(res);
+        console.log("..................");
+      });
+    }
+    localStorage.setItem("quiz", JSON.stringify(quiz));
+    console.log(quiz);
+  }, [quiz]);
+
+  const handleChooseAnswer = (e: React.MouseEvent<HTMLDivElement>) => {
+    const choiceId = e.currentTarget.dataset.id;
+    if (choiceId) {
+      answerQuestion(quiz.sessionId, choiceId);
+      setQuiz((prev) => ({ ...prev, answerId: choiceId }));
+    }
+  };
+
   return (
     <>
       <div className="h-full w-full grid grid-cols-1 grid-rows-12">
         <div className="bg-theme-second-bg row-start-1 row-end-8 flex items-center justify-center relative">
-          <UserAvatarContainer position="left" />
-          <UserAvatarContainer position="right" />
-          <div className="p-4">question</div>
+          {gameMode === "mp" && <UserAvatarContainer position="left" />}
+          <UserAvatarContainer position="right" user={user ?? undefined} />
+          <div className="p-4 text-2xl md:text-3xl font-medium">
+            {quiz.currentQuestion?.prompt || ""}
+          </div>
         </div>
         <div className="bg-theme-dark-bg row-start-8 row-end-13 grid grid-cols-2 grid-rows-2">
-          {quizs.map((item, i) => (
-            <QuizChoice tag={quizChoiceTagArr[i]}>{item.question}</QuizChoice>
-          ))}
+          {quiz.currentQuestion.choices &&
+            quiz.currentQuestion.choices.map((item, i) => (
+              <QuizChoice
+                onClick={handleChooseAnswer}
+                key={i}
+                tag={quizChoiceTagArr[i]}
+                data-id={item.choiceId}
+              >
+                {item.text}
+              </QuizChoice>
+            ))}
         </div>
       </div>
     </>
