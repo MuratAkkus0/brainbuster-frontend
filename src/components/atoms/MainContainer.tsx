e2d2708e93c1647ddbf1d28113aa2344d54a1471
Murat Akkus <akkusmurat123@gmail.com>
2025-10-14 21:52:47 +0200

    page animation added and user Dashboard ui improved.

--- src/components/atoms/MainContainer.tsx
+++ src/components/atoms/MainContainer.tsx
@@ -1,5 +1,9 @@
 import { cn } from "@/lib/utils";
-import type { ReactNode } from "react";
+import { useEffect, type ReactNode } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router";
+import type { AppDispatch, RootState } from "@/store/store";
+import { setIsLoading } from "@/store/slices/appSlice";
 
 interface MainContainerInterface {
   children: ReactNode;
@@ -9,11 +13,27 @@ export const MainContainer: React.FC<MainContainerInterface> = ({
   className,
   children,
 }) => {
+  const isLoading = useSelector((store: RootState) => store.app.isLoading);
+  const dispatch = useDispatch<AppDispatch>();
+  const location = useLocation();
+
+  useEffect(() => {
+    dispatch(setIsLoading(true));
+
+    const timeout = setTimeout(() => {
+      dispatch(setIsLoading(false));
+    }, 200);
+
+    return () => clearTimeout(timeout);
+  }, [location.pathname]);
   return (
     <>
       <div
         className={cn(
-          "w-full py-1 md:py-16 flex justify-center items-start bg-theme-second-bg overflow-y-auto",
+          "relative w-full py-1 md:py-16 flex justify-center items-start bg-theme-second-bg overflow-y-auto",
+          isLoading
+            ? "opacity-0 pointer-events-none"
+            : "opacity-100 transition-opacity duration-[170ms] pointer-events-auto",
           className
         )}
       >
--- src/components/molecules/UserInformationsCard.tsx
+++ src/components/molecules/UserInformationsCard.tsx
@@ -6,17 +6,19 @@ import {
 } from "@/components/ui/card";
 import { UserAvatar } from "../atoms/UserAvatar";
 import { useAuth } from "@/hooks";
+import { Edit } from "lucide-react";
 
 export const UserInformationsCard = () => {
   const user = useAuth();
   return (
-    <Card className="w-80">
+    <Card className="w-80 md:w-full h-full">
       <CardHeader className="flex gap-4 w-full">
         <UserAvatar className="bg-theme-second-bg p-2 size-32 rounded-lg" />
         <div>
           <CardTitle>{user.user.user?.user.username ?? ""}</CardTitle>
           <CardDescription>{user.user.user?.user.role ?? ""}</CardDescription>
         </div>
+        <Edit className="size-5" />
       </CardHeader>
     </Card>
   );
